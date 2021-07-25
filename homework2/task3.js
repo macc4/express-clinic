73770299f0a01ef4ab18b8f9957f9659fbede15a
macc4 <alekseileonenko98@gmail.com>
2021-07-25 16:33:55 +0300

    LESSON-2-3: Change arg names, add comments

--- homework2/task3.js
+++ homework2/task3.js
@@ -1,15 +1,21 @@
-const copy = function (n, x, y) {
-  if (n == 1) {
-    return Math.min(x, y);
+// implementation was created based on simple binary search
+// it iterates through the array of [0, ..., maximum time requried to make all of the copies)
+// and checks if average value is enough to make (copies - 1) (not including the first copy),
+// if no - use the left half of the array
+// if yes - use the right half, until we get to the optimal result
+
+const copy = function (copies, firstCopierSpeed, secondCopierSpeed) {
+  if (copies == 1) {
+    return Math.min(firstCopierSpeed, secondCopierSpeed);
   }
 
   let answer = 0;
   let lower = 0;
-  let upper = Math.max(x, y) * n;
+  let upper = Math.max(firstCopierSpeed, secondCopierSpeed) * copies;
 
   while (lower <= upper) {
     let mid = Math.floor((lower + upper) / 2);
-    if (mid / x + mid / y >= n - 1) {
+    if (mid / firstCopierSpeed + mid / secondCopierSpeed >= copies - 1) {
       answer = mid;
       upper = mid - 1;
     } else {
@@ -17,7 +23,7 @@ const copy = function (n, x, y) {
     }
   }
 
-  return answer + Math.min(x, y);
+  return answer + Math.min(firstCopierSpeed, secondCopierSpeed);
 };
 
 console.log(copy(50, 1, 2)); // 34
