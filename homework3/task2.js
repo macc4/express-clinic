31556584b0eac4fb510b7e97dd2dcdd9a1d72f0e
macc4 <alekseileonenko98@gmail.com>
2021-07-30 00:26:21 +0300

    LESSON-3-2: Move keys to properties

--- homework3/task2.js
+++ homework3/task2.js
@@ -20,72 +20,40 @@ class Calculator {
 class Converter {
   constructor(number) {
     this.number = number;
-  }
-
-  convertToRomanNumbers() {
-    if (isNaN(this.number)) return NaN;
-    if (this.number === 0 || this.number < 0) {
-      return 'There are no negative Roman Numerals, nor is there a Roman Numeral for zero.';
-    }
-    if (this.number > 3999) {
-      return 'The biggest number that can be formed in Roman Numerals is 3,999';
-    }
-    let digits = String(+this.number).split(''),
-      key = [
-        '',
-        'C',
-        'CC',
-        'CCC',
-        'CD',
-        'D',
-        'DC',
-        'DCC',
-        'DCCC',
-        'CM',
-        '',
-        'X',
-        'XX',
-        'XXX',
-        'XL',
-        'L',
-        'LX',
-        'LXX',
-        'LXXX',
-        'XC',
-        '',
-        'I',
-        'II',
-        'III',
-        'IV',
-        'V',
-        'VI',
-        'VII',
-        'VIII',
-        'IX',
-      ],
-      roman = '',
-      i = 3;
-    while (i--) roman = (key[+digits.pop() + i * 10] || '') + roman;
-    return Array(+digits.join('') + 1).join('M') + roman;
-  }
-
-  convertToArabicNumbers() {
-    if (isNaN(this.number)) return NaN;
-    let result = this.number.toString();
-    if (this.number < 0) {
-      result = result.substring(1) + '-';
-    }
-    let key = '٠١٢٣٤٥٦٧٨٩';
-    return result.replace(/\d/g, (symbol) => key[symbol]);
-  }
-
-  convertToMorseCode() {
-    if (isNaN(this.number)) return NaN;
-    let result = this.number.toString();
-    if (this.number < 0) {
-      result = '_...._' + result.substring(1);
-    }
-    let key = [
+    this.arabicKeys = '٠١٢٣٤٥٦٧٨٩';
+    this.romanKeys = [
+      '',
+      'C',
+      'CC',
+      'CCC',
+      'CD',
+      'D',
+      'DC',
+      'DCC',
+      'DCCC',
+      'CM',
+      '',
+      'X',
+      'XX',
+      'XXX',
+      'XL',
+      'L',
+      'LX',
+      'LXX',
+      'LXXX',
+      'XC',
+      '',
+      'I',
+      'II',
+      'III',
+      'IV',
+      'V',
+      'VI',
+      'VII',
+      'VIII',
+      'IX',
+    ];
+    this.morseKeys = [
       '-----',
       '.----',
       '..---',
@@ -97,7 +65,41 @@ class Converter {
       '---..',
       '----.',
     ];
-    return result.replace(/\d/g, (symbol) => key[symbol]);
+  }
+
+  convertToRomanNumbers() {
+    if (isNaN(this.number)) return NaN;
+    if (this.number === 0 || this.number < 0) {
+      return 'There are no negative Roman Numerals, nor is there a Roman Numeral for zero.';
+    }
+    if (this.number > 3999) {
+      return 'The biggest number that can be formed in Roman Numerals is 3,999';
+    }
+
+    let digits = String(+this.number).split('');
+    let roman = '';
+    let i = 3;
+
+    while (i--) roman = (this.romanKeys[+digits.pop() + i * 10] || '') + roman;
+    return Array(+digits.join('') + 1).join('M') + roman;
+  }
+
+  convertToArabicNumbers() {
+    if (isNaN(this.number)) return NaN;
+    let result = this.number.toString();
+    if (this.number < 0) {
+      result = result.substring(1) + '-';
+    }
+    return result.replace(/\d/g, (symbol) => this.arabicKeys[symbol]);
+  }
+
+  convertToMorseCode() {
+    if (isNaN(this.number)) return NaN;
+    let result = this.number.toString();
+    if (this.number < 0) {
+      result = '_...._' + result.substring(1);
+    }
+    return result.replace(/\d/g, (symbol) => this.morseKeys[symbol]);
   }
 }
 
