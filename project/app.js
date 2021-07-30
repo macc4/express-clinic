ee427bd3597ca861338c709641dd800d20710baa
Aleksei Leonenko <alekseileonenko98@gmail.com>
2021-07-30 17:55:18 +0300

    LESSON-4: Add basic logic to the page

--- project/app.js
+++ project/app.js
@@ -1,34 +1,101 @@
 document.addEventListener('DOMContentLoaded', () => {
   let patients = [];
 
-  const patientForm = document.getElementById('patient-form'),
+  const newPatientForm = document.getElementById('patient-form'),
     doctorResolutionForm = document.getElementById('doctor-resolution-form'),
-    currentPatient = document.getElementById('patient-current'),
-    doctorPatient = document.getElementById('doctor-current');
+    currentPatientTag = document.getElementById('patient-current'),
+    doctorPatientTag = document.getElementById('doctor-current'),
+    nextPatientButton = document.getElementById('doctor-next-patient'),
+    patientSearchResolutionName = document.getElementById(
+      'patient-search-form'
+    ),
+    patientResolutionSearchButton = document.getElementById(
+      'patient-resolution-button'
+    ),
+    patientResolutionField = document.getElementById(
+      'patient-resolution-field'
+    ),
+    doctorSearchResolutionName = document.getElementById('doctor-search-form'),
+    doctorResolutionField = document.getElementById('doctor-resolution-field'),
+    doctorResolutionSearchButton = document.getElementById(
+      'doctor-resolution-button-search'
+    ),
+    doctorResolutionDeleteButton = document.getElementById(
+      'doctor-resolution-button-delete'
+    );
 
-  const showCurrentPatient = function (database, parent) {
-    const currentPatientName = database[0][0];
-    parent.innerHTML = `${currentPatientName}`;
+  let currentPatientIndex = undefined;
+
+  const showCurrentPatient = function (patient, parentObject) {
+    parentObject.innerHTML = `${patient}`;
   };
 
-  const showResolutionByPatientName = function () {};
+  const selectTheNextPatient = function (database) {
+    // find the first patient without the resolution
+    currentPatientIndex = database.findIndex((element) => element[1] === '');
+    if (currentPatientIndex === -1) {
+      return alert('There are no patients left!');
+    }
+    // show the name on the webpage
+    showCurrentPatient(database[currentPatientIndex][0], currentPatientTag);
+    showCurrentPatient(database[currentPatientIndex][0], doctorPatientTag);
+    console.log(database);
+  };
 
   const addPatientToDatabase = function (database, patient) {
-    database.push([patient, '']);
+    let duplicatePatient = database.some((el) => el[0] === patient);
+
+    if (!duplicatePatient) {
+      database.push([patient, '']);
+    } else {
+      alert('Database already contains this patient!');
+    }
+    console.log(database);
+  };
+
+  const deletePatientFromDatabase = function (database, patient) {
+    const currentPatientSearchIndex = database.findIndex(
+      (element) => element[0] === patient
+    );
+    if (currentPatientSearchIndex === -1) {
+      return alert('There is no patient with such name!');
+    }
+    database.splice(currentPatientSearchIndex, 1);
+    alert(`The ${patient} patient has been deleted!`);
     console.log(database);
   };
 
   const addResolutionToPatient = function (database, resolution) {
-    const currentPatientValue = database.findIndex(
-      (element) => element[0] === doctorPatient.innerHTML
-    );
-    database[currentPatientValue][1] = resolution;
+    if (currentPatientIndex === -1 || currentPatientIndex === undefined) {
+      return alert('Please, select the patient first!');
+    }
+    // check if resolution is empty
+    if (database[currentPatientIndex][1] === '') {
+      database[currentPatientIndex][1] = resolution;
+    } else {
+      database[currentPatientIndex][1] += ` \n ` + resolution;
+    }
+    console.log(database);
   };
 
-  const moveToTheNextPatient = function () {};
-  const deletePatientFromDatabase = function () {};
+  const showResolutionByPatientName = function (
+    database,
+    patient,
+    parentObject
+  ) {
+    const currentPatientSearchIndex = database.findIndex(
+      (element) => element[0] === patient
+    );
+    if (currentPatientSearchIndex === -1) {
+      return alert('There is no patient with such name');
+    }
+    parentObject.innerHTML = database[currentPatientSearchIndex][1];
+    console.log(database);
+  };
 
-  patientForm.addEventListener('submit', (event) => {
+  // logic of the page
+
+  newPatientForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
     const patientInput = document.getElementById('patient-form-input');
@@ -36,10 +103,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     addPatientToDatabase(patients, patientName);
 
-    document.getElementById('patient-form').reset();
+    newPatientForm.reset();
+  });
 
-    showCurrentPatient(patients, currentPatient);
-    showCurrentPatient(patients, doctorPatient);
+  patientResolutionSearchButton.addEventListener('click', () => {
+    const patientName = patientSearchResolutionName.value;
+    showResolutionByPatientName(patients, patientName, patientResolutionField);
+    patientResolutionField.reset();
+  });
+
+  nextPatientButton.addEventListener('click', () => {
+    selectTheNextPatient(patients);
   });
 
   doctorResolutionForm.addEventListener('submit', (event) => {
@@ -47,5 +121,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const resolutionInput = document.getElementById('resolution-input');
     const resolutionText = resolutionInput.value;
+
+    addResolutionToPatient(patients, resolutionText);
+
+    doctorResolutionForm.reset();
+  });
+
+  doctorResolutionSearchButton.addEventListener('click', () => {
+    const patientName = doctorSearchResolutionName.value;
+    showResolutionByPatientName(patients, patientName, doctorResolutionField);
+
+    doctorSearchResolutionName.reset();
+  });
+
+  doctorResolutionDeleteButton.addEventListener('click', () => {
+    const patientName = doctorSearchResolutionName.value;
+    deletePatientFromDatabase(patients, patientName);
+
+    doctorSearchResolutionName.reset();
   });
 });
