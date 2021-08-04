bb98b538527c4402ed43409f31f01c39b08898b1
macc4 <alekseileonenko98@gmail.com>
2021-08-04 10:44:45 +0300

    LESSON-5: Remove the server-based version

--- project1/public/index.js
+++
@@ -1,7 +0,0 @@
-import QueueController from './controllers/queueController.js';
-import QueueModel from './models/queueModel.js';
-import QueueView from './views/queueView.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const queue = new QueueController(new QueueModel(), new QueueView());
-});
